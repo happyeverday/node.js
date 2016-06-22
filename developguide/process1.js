e1ac4a97f5a708817a2b70b374c19f09b0785863
happyeverday <2277630263@qq.com>
2016-06-22 17:37:48 +0800

    haha

--- developguide/process1.js
+++ developguide/process1.js
@@ -1,3 +1,18 @@
 /**
  * Created by JNC on 2016/6/22.
  */
+f = function(b) {
+    console.log(b);
+    if(b==1001){
+        return;
+    }
+    return process.nextTick(function() {
+        return f(b + 1);
+    });
+};
+
+setImmediate(function () {
+    console.log('------------------------------------');
+});
+
+f(1);
